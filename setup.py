1f0c5699f3dfebe510aef123353c5dde949dc71e
Dan Prince <dprince@redhat.com>
2013-06-19 13:52:41 -0400

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="iboot",
+    version="0.1.0",
+    author="Luke Fitzgerald",
+    author_email="",
+    description=("Python library to control iBoot."),
+    license="BSD",
+    keywords="python iboot",
+    url="https://github/darkip/iboot",
+    packages=['iboot'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
